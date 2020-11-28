08060dd9fd41f1e9e63553c9c7ff3728c27efe56
HamzaShabbirChohan <hamzashabbirchohan333@gmail.com>
2020-11-28 21:11:43 +0500

    hello world

---
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Email;
+    private EditText password;
+    private Button   login;
+    private  TextView info;
+    SharedPreferences sharesprefences;
+    private static String namee="hamza";
+    private static String Key_name="name";
+    private static String key_email="email";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Email=(EditText)findViewById(R.id.editText);
+        password=(EditText)findViewById(R.id.editText3);
+        login=(Button)findViewById(R.id.button);
+        info=(TextView)findViewById(R.id.textView2);
+        sharesprefences=getSharedPreferences(namee,MODE_PRIVATE);
+        String name=sharesprefences.getString(Key_name,null);
+        String passwordd=sharesprefences.getString(key_email,null);
+        if(name!=null && password!=null)
+        {
+            Email.setText(name);
+            password.setText(passwordd);
+        }
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String name=Email.getText().toString();
+                String passwordd= password.getText().toString();
+
+
+                if(name.isEmpty() || passwordd.isEmpty())
+                {
+                    Toast.makeText(MainActivity.this,"please enter your name or id",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+
+                    SharedPreferences.Editor editor=sharesprefences.edit();
+                    editor.putString(Key_name,name);
+                    editor.putString(key_email,passwordd);
+                    editor.apply();
+
+                        Toast.makeText(MainActivity.this, "succesfully movred to nexr secreen",
+                                Toast.LENGTH_LONG).show();
+
+                        Intent intent = new Intent(MainActivity.this, signup.class);
+                        intent.putExtra("name", name);
+                        intent.putExtra("password", passwordd);
+                        startActivity(intent);
+                        Toast.makeText(MainActivity.this, "name   " + name + '\n' + " your password" + passwordd, Toast.LENGTH_SHORT).show();
+                    }
+
+
+            }
+
+
+        });
+
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/testapp/signup.java
@@ -0,0 +1,58 @@
+package com.example.testapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class signup extends AppCompatActivity {
+
+    private EditText ename;
+    private EditText email;
+    private EditText password;
+    private Button   btn;
+    private TextView infoo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        ename=(EditText)findViewById(R.id.editText);
+        email=(EditText)findViewById(R.id.editText4);
+        password=(EditText)findViewById(R.id.editText5);
+        btn=(Button)findViewById(R.id.button3);
+       infoo=(TextView)findViewById(R.id.textView3);
+
+       final String name =   getIntent().getStringExtra("name");
+       final String passwords= getIntent().getStringExtra("password");
+       ename.setText(name);
+       password.setText(passwords);
+
+
+    btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String namee= ename.getText().toString();
+                String mail=email.getText().toString();
+                String pasword=password.getText().toString();
+                if(namee.isEmpty() || mail.isEmpty() || pasword.isEmpty())
+                {
+                    Toast.makeText(signup.this,"please enter your all data",Toast.LENGTH_LONG).show();
+                }else{
+
+                }
+                {
+                    Toast.makeText(signup.this,"name   "+namee +'\n'+" your email  "+mail+'\n'+"youd password "
+                            +password,Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        });
+
+
+    }
+}
